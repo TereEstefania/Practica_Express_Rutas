f175398aee415ef44ab67bab9c5191abda984ba4
TereEstefania <fernandez.e.tere@gmail.com>
2025-05-13 23:45:53 -0300

    comienzo a desarrollar los servicios

---
+++ mocked/lenguajes.js
@@ -0,0 +1,102 @@
+let infoLenguajes = {
+    frontend:[
+        {
+            id: 1,
+            nombre: "JavaScript",
+            turno: "mañana",
+            comision: "A",
+            cantidadAlumnos: 30
+        },
+        {
+            id: 2,
+            nombre: "JavaScript",
+            turno: "tarde",
+            comision: "B",
+            cantidadAlumnos: 45
+        },
+        {
+            id: 3,
+            nombre: "JavaScript",
+            turno: "noche",
+            comision: "C",
+            cantidadAlumnos: 33
+        },
+        {
+            id: 4,
+            nombre: "JavaScript",
+            turno: "mañana",
+            comision: "A",
+            cantidadAlumnos: 65
+        },
+        {
+            id: 4,
+            nombre: "JavaScript",
+            turno: "mañana",
+            comision: "C",
+            cantidadAlumnos: 12
+        },
+        {
+            id: 5,
+            nombre: "HTML",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 22
+        },
+        {
+            id: 2,
+            nombre: "HTML",
+            turno: "mañana",
+            comision: "A",
+            cantidadAlumnos: 43
+        },
+        {
+            id: 2,
+            nombre: "HTML",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 30
+        },
+        {
+            id: 3,
+            nombre: "CSS",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 30
+        }
+    ],
+    backend:[
+        {
+            id: 1,
+            nombre: "JavaScript",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 30
+        },
+        {
+            id: 2,
+            nombre: "TypeScript",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 30
+        },
+        {
+            id: 3,
+            nombre: "C-Sharp",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 30
+        },
+        {
+            id: 4,
+            nombre: "PHP",
+            turno: "noche",
+            comision: "A",
+            cantidadAlumnos: 30
+        }
+    ]
+}
+
+module.exports = {
+    frontend : infoLenguajes.frontend, 
+    backend : infoLenguajes.backend
+};
